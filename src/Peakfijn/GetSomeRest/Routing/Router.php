8975645bca35060f876d24b49e3b52c9c3493c36
Cedric van Putten <cedric@peakfijn.nl>
2014-06-12 10:08:27 +0200

    Wrapped the Router::dispatch to allow the use of HttpExceptions

--- src/Peakfijn/GetSomeRest/Routing/Router.php
+++ src/Peakfijn/GetSomeRest/Routing/Router.php
@@ -1,6 +1,8 @@
 <?php namespace Peakfijn\GetSomeRest\Routing;
 
 use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class Router extends \Illuminate\Routing\Router {
 
@@ -16,6 +18,50 @@ class Router extends \Illuminate\Routing\Router {
 	public function api( $version, Closure $callback )
 	{
 		return $this->group(['api' => true, 'prefix' => $version], $callback);
+	}
+
+	/**
+	 * Try and execute the requested route.
+	 * If the requested route is an API route,
+	 * catch all HttpExceptions and make it a nice response.
+	 *
+	 * @param  Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function dispatch( Request $request )
+	{
+		// If the route is not an API route,
+		// dont modify the response
+		if( !$this->isApiRequest($request) )
+		{
+			return parent::dispatch($request);
+		}
+
+		try
+		{
+			$response = parent::dispatch($request);
+		}
+		// Only catch HttpExceptions,
+		// other exceptions should still be thrown.
+		// Like the RuntimeException for example.
+		catch( HttpExceptionInterface $exception )
+		{
+			$response = new Response('error');
+		}
+
+		return $response;
+	}
+
+
+	/**
+	 * Detect if the given request is meant for an API route.
+	 * 
+	 * @param  \Illuminate\Http\Request $request
+	 * @return boolean
+	 */
+	protected function isApiRequest( Request $request )
+	{
+		return !!preg_match('/v[0-9]+/', $request->segment(1));
 	}
 
 }
