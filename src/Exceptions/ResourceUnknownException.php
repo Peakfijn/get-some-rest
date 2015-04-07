f0867f0c2874bc0ea904b5cdcbdbb7d092208b47
Cedric van Putten <cedric@peakfijn.nl>
2015-04-07 15:01:18 +0200

    Rewired resource unknown exception to make the requested resource name optional.

--- src/Exceptions/ResourceUnknownException.php
+++ src/Exceptions/ResourceUnknownException.php
@@ -2,11 +2,14 @@
 
 class ResourceUnknownException extends RestException
 {
-    public function __construct($resource)
+    public function __construct($resource = '')
     {
-        parent::__construct(
-            404,
-            'Could not find the requested resource "' . $resource . '".'
-        );
+        $message = 'Could not find the requested resource';
+
+        if (!empty($resource)) {
+            $message .= ' "' . $resource . '"';
+        }
+
+        parent::__construct(404, $message . '.');
     }
 }
